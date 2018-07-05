bbe0c07d5b121a320374b3c6a82db3a12aa7f154
Rohit Mohan <rohit.mohan@gmail.com>
2018-07-05 08:07:12 -0400

    store video urls to database

--- EdFlixViewModel.js
+++ EdFlixViewModel.js
@@ -5,51 +5,103 @@ function EdFlixViewModel()
     self.url = ko.observable("");
     self.title = ko.observable("");
 
-    self.relevance = ko.observable(0); // 1-5
+    self.rating = ko.observable(0); // 1-5
     self.grade = ko.observable("K"); // K-12
-    self.easeOfUnderstanding = ko.observable(5); // 1-5
+    self.intuitive = ko.observable(5); // 1-5
 
     self.videoAdded = ko.observable(false);
 
-    self.addVideo = function()
+    self.addVideo = function(event)
     {
         console.log("adding video '" + self.title() + "'");
+
+        video = {
+            url: self.url(),
+            title: self.title(),
+            grade: self.grade(),
+            rating: self.rating(),
+            intuitive: self.intuitive()
+        };
+
+        localStorage.setItem(self.url(), JSON.stringify(video));
+
         self.videoAdded(true);
+
+        event.preventDefault();
     };
 
-    self.updateVideo = function()
+    self.updateVideo = function(event)
     {
         console.log("updating video '" + self.title() + "'");
+
+        var video = JSON.parse(localStorage.getItem(self.url()));
+
+        if (!video)
+        {
+            return;
+        }
+
+        video.grade = self.grade();
+        video.rating = self.rating();
+        video.intuitive = self.intuitive();
+
+        localStorage.setItem(self.url(), video);
+
+        event.preventDefault();
     }
 
     self.applyBindings = function()
     {
+        console.log("applying bindings");
+
         ko.applyBindings(self, document.getElementById("edflix"));
     };
 
     self.attachEventHandlers = function()
     {
+        console.log("attaching event handlers");
+
         var addVideoButton = document.getElementById("add-video");
-        addVideoButton.addEventListener("click", self.addVideo);
+        if (addVideoButton)
+        {
+            addVideoButton.addEventListener("click", self.addVideo);
+        }
 
         var updateVideoButton = document.getElementById("update-video");
-        updateVideoButton.addEventListener("click", self.updateVideo);
+        if (updateVideoButton)
+        {
+            updateVideoButton.addEventListener("click", self.updateVideo);
+        }
     };
 
-    self.init = function ()
+    self.init = function()
     {
-        console.log("applying bindings");
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            var url = tabs[0].url;
 
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            // if the video is already added, note that. this will
+            // cause the ui to show the update button instead of
+            // the add button
+            if (localStorage.getItem(url))
+            {
+                self.videoAdded(true);
+
+                return;
+            }
+
             self.url(tabs[0].url);
             self.title(tabs[0].title);
             self.grade("K");
-            self.relevance(5);
-            self.easeOfUnderstanding(5);
+            self.rating(5);
+            self.intuitive(5);
         });
 
-        self.applyBindings();
+        // attach the click event handlers to the buttons before binding the
+        // elements to this view model because once the binding code runs,
+        // the update button will be removed from the dom and will not get
+        // its event handler
         self.attachEventHandlers();
+        self.applyBindings();
     };
 
     self.init();
